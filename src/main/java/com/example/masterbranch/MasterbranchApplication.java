c584496a8ea1a22cae2bfc84012974a7eff4669f
tahsinSalamanca <gokmentahsin@gmail.com>
2023-10-26 18:14:03 +0200

    Added swagger

--- src/main/java/com/example/masterbranch/MasterbranchApplication.java
+++ src/main/java/com/example/masterbranch/MasterbranchApplication.java
@@ -3,6 +3,8 @@ package com.example.masterbranch;
 import com.example.masterbranch.config.DynamoDBConfig;
 import com.example.masterbranch.entity.Person;
 import com.example.masterbranch.repository.PersonRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +19,7 @@ import java.util.List;
 @SpringBootApplication
 @RestController
 @Import({DynamoDBConfig.class})
+@Api(tags = "Person Api")
 @RequestMapping("/api")
 public class MasterbranchApplication {
 
@@ -24,31 +27,37 @@ public class MasterbranchApplication {
 	private PersonRepository personRepository;
 
 	@PostMapping("/addPerson")
+	@ApiOperation(value = "Add a new person")
 	public Person addPerson(@RequestBody Person person){
 		return personRepository.addPerson(person);
 	}
 
 	@GetMapping("/getPerson/{personId}")
+	@ApiOperation(value = "Call one person by id")
 	public Person getPerson(@PathVariable String  personId){
 		return personRepository.findPersonByPersonId(personId);
 	}
 
 	@GetMapping("/getAllPeople")
+	@ApiOperation(value = "Call All People")
 	public List<Person> getAllPeople(){
 		return personRepository.findAllPerson();
 	}
 
 	@DeleteMapping("/deletePerson/{personId}")
+	@ApiOperation(value = "Delete one person by id")
 	public String deletePerson(@PathVariable String  personId){
 		return personRepository.deletePerson(personId);
 	}
 
 	@PutMapping("/updatePerson")
+	@ApiOperation(value = "Update person")
 	public String editPerson(@RequestBody Person  person){
 		return personRepository.editPerson(person);
 	}
 
 	@PatchMapping("/partialUpdate/{personId}")
+	@ApiOperation(value = "Update person partially")
 	public String editPerson(@PathVariable String personId, @Validated  @RequestBody Person  person){
 		return personRepository.patchUpdate(personId,person);
 	}
